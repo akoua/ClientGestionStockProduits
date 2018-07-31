07341c050fe02a46cb9efc753bc1ca8c89742585
akoua <akouayao225@gmail.com>
2018-07-31 07:23:35 +0000

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { ContentComponent } from './content/content.component';
 import { ProduitComponent } from './produit/produit.component';
 import { ProduitService } from './services/produit.service';
 import { Produit } from './shared/produit';
@@ -14,13 +11,10 @@ import { Produit } from './shared/produit';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    SidebarComponent,
-    ContentComponent,
     ProduitComponent
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule
   ],
   providers: [ProduitService],
   bootstrap: [AppComponent]
